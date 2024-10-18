995e76202be260c08e76fc870501f7c19bc96507
Backstage Test <yurarsit@gmail.com>
2024-10-18 16:02:12 +0400

    Add GCP bucket my-test-bucket-2

--- projects/project-A/buckets/my-test-bucket-2/main.tf
+++ projects/project-A/buckets/my-test-bucket-2/main.tf
@@ -1,11 +1,10 @@
 provider "google" {
   project     = "project-A"
-  region      = "us-west1"
 }
 
 resource "google_storage_bucket" "auto-expire" {
   name          = my-test-bucket-2
-  location      = US-WEST2
+  location      = US-WEST1
   force_destroy = true
 
   public_access_prevention = "enforced"
